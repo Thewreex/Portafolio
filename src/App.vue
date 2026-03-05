c4d3c1e274ddd50f144a5343ef8881ae956f51fb
JuanPablo <juanpablodiaz2518@gmail.com>
2026-03-05 20:34:40 -0300

    Estructura del hero section

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <appHeader />
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
-  <h1>1</h1>
+  <appHeader id="appHeader" />
+  <heroSection id="heroSection" />
 </template>
 
 <script setup>
 import appHeader from './components/appHeader.vue';
+import heroSection from './components/heroSection.vue';
 </script>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+#heroSection,
+#appHeader {
+  // border: 2px solid red;
+}
+</style>
---
+++ src/components/heroSection.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container d-flex justify-content-between p-5 contenedor-hero mt-5">
+        <div>
+            <img src="https://placehold.co/375x375" alt="">
+        </div>
+        <div class="contenedor-texto">
+            <div class="d-flex align-items-center flex-nowrap">
+                <p class="mb-0 text-nowrap">Hola, Soy Juan Pablo</p>
+                <div class="linea mx-2"></div>
+            </div>
+
+            <div class="d-flex mt-2 align-items-end gap-3 flex-column">
+                <div class="d-flex gap-3">
+                    <h1 class="texto-titulo">Desarrollador <span class="texto-titulo">FullStack</span>
+                    </h1>
+                </div>
+                <p class="text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptate
+                    exercitationem esse facilis
+                    praesentium eum nemo officiis dolor eius, eveniet aut tempora id laudantium animi voluptates,
+                    perspiciatis tenetur quo odit.</p>
+
+                <div class="d-flex gap-3 align-items-center border border-3 rounded-5 p-2">
+                    Abierto a nuevas oportunidades
+                    <div class="circulo rounded-circle bg-success"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped lang="scss">
+.circulo {
+    width: 20px;
+    height: 20px;
+}
+
+.texto-titulo {
+    font-size: 60px;
+}
+
+.contenedor-texto {
+    width: 650px;
+}
+
+
+.contenedor-hero {
+    height: 75vh;
+}
+
+.linea {
+    height: 1px;
+    flex: 1;
+    background-color: black;
+}
+</style>
