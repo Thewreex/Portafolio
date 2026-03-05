5310845a7b79c30bdb8e0094c242c8c2b42eea98
JuanPablo <juanpablodiaz2518@gmail.com>
2026-03-05 16:41:35 -0300

    Estructura del header lista!

---
+++ src/components/appHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header-nav px-5 gap-5 d-flex align-items-center justify-content-between mx-auto mt-4 bg-primary">
+        <div class="d-flex gap-5">
+            <font-awesome-icon icon="user" class="icono-header" />
+            <font-awesome-icon icon="folder" class="icono-header" />
+            <font-awesome-icon icon="screwdriver-wrench" class="icono-header" />
+            <font-awesome-icon icon="envelope" class="icono-header" />
+        </div>
+
+        <div class="d-flex gap-5">
+            <font-awesome-icon :icon="['fab', 'github']" class="icono-header" />
+            <font-awesome-icon :icon="['fab', 'linkedin']" class="icono-header" />
+            <font-awesome-icon icon="file" class="icono-header" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.icono-header {
+    height: 35px;
+    width: 50px;
+}
+
+.header-nav {
+    width: 50vw;
+    height: 65px;
+    border-radius: 100px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/styles/main.scss";
 
-createApp(App).mount('#app')
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faEnvelope,
+  faFolder,
+  faScrewdriverWrench,
+  faFile,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faUser,
+  faFolder,
+  faScrewdriverWrench,
+  faEnvelope,
+  faGithub,
+  faLinkedin,
+  faFile,
+);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
