01fc77a674070e50628886edc3ca4ece801b0fd1
JuanPablo <juanpablodiaz2518@gmail.com>
2026-03-08 20:35:00 -0300

    Avances en la responsividad del header

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="sticky-top header-nav px-5 gap-5 d-flex align-items-center justify-content-between mx-auto bg-primary">
-        <div class="d-flex gap-5">
+    <div class="header-nav d-flex flex-column  justify-content-lg-between flex-lg-row bg-primary">
+        <div class="d-flex flex-column flex-lg-row header-secciones ">
             <font-awesome-icon icon="user" class="icono-header" />
             <font-awesome-icon icon="folder" class="icono-header" />
             <font-awesome-icon icon="screwdriver-wrench" class="icono-header" />
             <font-awesome-icon icon="envelope" class="icono-header" />
         </div>
 
-        <div class="d-flex gap-5">
-            <font-awesome-icon :icon="['fab', 'github']" class="icono-header" />
-            <font-awesome-icon :icon="['fab', 'linkedin']" class="icono-header" />
-            <font-awesome-icon icon="file" class="icono-header" />
+        <div class="d-flex flex-column flex-lg-row header-secciones">
+            <font-awesome-icon :icon="['fab', 'github']" class="icono-header " />
+            <font-awesome-icon :icon="['fab', 'linkedin']" class="icono-header " />
+            <font-awesome-icon icon="file" class="icono-header " />
         </div>
     </div>
 </template>
@@ -19,15 +19,36 @@
 </script>
 
 <style scoped>
-.icono-header {
-    height: 35px;
-    width: 50px;
+.header-nav {
+    padding: 30px 15px;
+    align-items: center;
+    justify-content: space-between;
+    height: 70vh;
+    position: fixed;
+    right: 1.5rem;
+    top: 50%;
+    transform: translateY(-50%);
+    width: auto;
+    border-radius: 50px;
 }
 
-.header-nav {
-    width: 50vw;
-    height: 65px;
-    border-radius: 100px;
-    top: 1.5rem
+.header-secciones {
+    gap: clamp(0.5rem, 2.7vw, 3.5rem);
+}
+
+.icono-header {
+    font-size: clamp(1.4rem, 2.1vw, 3rem);
+}
+
+@media (min-width: 992px) {
+    .header-nav {
+        top: 1.5rem;
+        left: 50%;
+        right: auto;
+        transform: translateX(-50%);
+        width: 50%;
+        height: auto;
+        padding: 15px clamp(20px, 5vw, 100px);
+    }
 }
 </style>
